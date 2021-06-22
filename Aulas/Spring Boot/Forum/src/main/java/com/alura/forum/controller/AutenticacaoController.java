3afe33922732baf991a2c5201a78625a28c1e99d
Caico Costa <sales.c.costa@gmail.com>
2021-06-22 10:03:57 -0300

    Implementado uma configuração para restrições de acesso baseadas em perfís de acesso. Implementado um sistema de profiles para testes em ambiente de desenvolvimento.

--- Aulas/Spring Boot/Forum/src/main/java/com/alura/forum/controller/AutenticacaoController.java
+++ Aulas/Spring Boot/Forum/src/main/java/com/alura/forum/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package com.alura.forum.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,6 +20,7 @@ import com.alura.forum.dto.TokenDTO;
 
 @RestController
 @RequestMapping("/auth")
+@Profile("prod")
 public class AutenticacaoController {
 	
 	@Autowired
