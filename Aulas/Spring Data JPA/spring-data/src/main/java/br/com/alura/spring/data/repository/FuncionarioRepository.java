1a4f723779c93bc5261d01a250b29125464d564f
Caico Costa <sales.c.costa@gmail.com>
2021-06-23 15:48:14 -0300

    Implementação de relatórios Buscas utilizando querys nativas e JPQL

--- Aulas/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ Aulas/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,11 +1,25 @@
 package br.com.alura.spring.data.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+	
+	List<Funcionario> findByNome(String nome);
+	
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario AND f.dataContratacao = :data")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+	
+	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data",
+			nativeQuery = true)
+	List<Funcionario> findDataContratacaoMaior(LocalDate data);
 
 }
